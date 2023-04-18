b7bf6bc2efdc59bcb85029cbebad85f06974dac6
Toru Kobayashi <koba0004@gmail.com>
2023-04-18 23:57:46 +0900

    feat: create HTTPClient for sending emails

--- src/configuration.rs
+++ src/configuration.rs
@@ -57,6 +57,7 @@ impl DatabaseSettings {
 pub struct EmailClientSettings {
     pub base_url: String,
     pub sender_email: String,
+    pub authorization_token: Secret<String>,
 }
 
 impl EmailClientSettings {
