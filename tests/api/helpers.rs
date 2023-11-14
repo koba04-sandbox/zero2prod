c60bbd7ff2234fe895ea5ba236591a38a0d81e35
Toru Kobayashi <koba0004@gmail.com>
2023-11-14 22:24:10 +0900

    next 10.7.5.3

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -124,6 +124,16 @@ impl TestApp {
             .await
             .expect("Failed to execute request.")
     }
+    pub async fn get_admin_dashboard(&self) -> String {
+        self.api_client
+            .get(&format!("{}/admin/dashboard", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+            .text()
+            .await
+            .unwrap()
+    }
 }
 
 static TRACING: Lazy<()> = Lazy::new(|| {
